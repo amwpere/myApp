c273ba3140617a6681b6a89e8f551882ca679a15
Nattakarn O <nattakarn.or@ku.th>
2021-07-01 22:06:20 +0700

    Update delete text in App.java

--- src/myApp/App.java
+++ src/myApp/App.java
@@ -2,10 +2,7 @@ package myApp;
 
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello Master");
-=======
         System.out.println("Hello Feature2");
->>>>>>> feature2
     }
 }

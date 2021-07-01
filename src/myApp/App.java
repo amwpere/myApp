eafa1ea3b9e0a887fee0ccfc1ef1d576d3d348b3
Nattakarn O <nattakarn.or@ku.th>
2021-07-01 21:24:35 +0700

    Update App.java by feature2

--- src/myApp/App.java
+++ src/myApp/App.java
@@ -2,6 +2,6 @@ package myApp;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Feature2");
     }
 }
